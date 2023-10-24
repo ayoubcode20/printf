eb48f673170754c8257b4ea7a346f1b2f1de4822
Ayoub <aoumraniayoub658@gmail.com>
2023-10-24 07:08:13 -0700

    FIX in printf project

--- print_number.c
+++ print_number.c
@@ -8,7 +8,7 @@
 */
 int _isdigit(int c)
 {
-	return (c >= 48 && c <= 57);
+	return (c >= '0' && c <= '9');
 }
 
 /**
@@ -19,7 +19,7 @@ int _isdigit(int c)
 */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (*s++)
 		i++;
